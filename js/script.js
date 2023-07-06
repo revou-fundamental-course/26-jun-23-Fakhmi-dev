a3f691ac265935d0fbb314b84619d7192446875d
Fakhmi-dev <fakhmianshary@gmail.com>
2023-07-06 21:23:50 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toogle
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu'). onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Klik di luar 
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))  {
+        navbarNav.classList.remove('active');
+    } 
+})
+
